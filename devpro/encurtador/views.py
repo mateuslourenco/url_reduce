e6f7caa32dae09894ddd62bc8f1a650c63874baa
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-02-23 15:33:06 -0300

    criado pagina de relatorios

--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 from devpro.encurtador.models import UrlRedirect
 
@@ -6,3 +6,13 @@ from devpro.encurtador.models import UrlRedirect
 def redirecionar(request, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
     return redirect(url_redirect.destino)
+
+
+def relatorios(request, slug):
+    url_redirect = UrlRedirect.objects.get(slug=slug)
+    url_reduzida = request.build_absolute_uri(f'/{slug}')
+    ctx = {
+        'reduce': url_redirect,
+        'url_reduzida': url_reduzida,
+    }
+    return render(request, 'encurtador/relatorio.html', ctx)
