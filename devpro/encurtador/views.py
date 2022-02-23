abc2fb7bda8c72df92112bb8a7cac52ea9aaee94
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-02-23 16:06:42 -0300

    criado modelo de url log

--- devpro/encurtador/views.py
+++ devpro/encurtador/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import redirect, render
 
-from devpro.encurtador.models import UrlRedirect
+from devpro.encurtador.models import UrlRedirect, UrlLog
 
 
 def redirecionar(request, slug):
     url_redirect = UrlRedirect.objects.get(slug=slug)
+    UrlLog.objects.create(
+        origem=request.META.get('HTTP_REFERER'),
+        user_agent=request.META.get('HTTP_USER_AGENT'),
+        host=request.META.get('HTTP_HOST'),
+        ip=request.META.get('REMOTE_ADDR'),
+        url_redirect=url_redirect
+    )
     return redirect(url_redirect.destino)
 
 
