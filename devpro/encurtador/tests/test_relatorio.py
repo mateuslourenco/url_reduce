5d69f14f2f1cb67646b25d4053a7bd5b4f491e0f
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-02-23 15:41:01 -0300

    implementado e atualizado testes relatorios

--- devpro/encurtador/tests/test_relatorio.py
+++ devpro/encurtador/tests/test_relatorio.py
@@ -1,11 +1,29 @@
 import pytest
 from django.urls import reverse
+from model_bakery import baker
+
+from devpro.django_assertions import assert_contains
+from devpro.encurtador.models import UrlRedirect
 
 
 @pytest.fixture
-def resp(client):
-    return client.get(reverse('relatorios', kwargs={'slug': 'slug'}))
+def url_redirect(db):
+    redirecionador = baker.make(UrlRedirect)
+    return redirecionador
+
+
+@pytest.fixture
+def resp(client, url_redirect):
+    return client.get(reverse('relatorios', kwargs={'slug': url_redirect.slug}))
 
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+def test_url_original_presente(resp, url_redirect):
+    assert_contains(resp, url_redirect.destino)
+
+
+def test_url_reduzida_presente(resp, url_redirect):
+    assert_contains(resp, f'/{url_redirect.slug}')
