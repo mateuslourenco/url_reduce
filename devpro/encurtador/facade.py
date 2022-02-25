877ff17d8378362b72a6dd8a9f32440cb0757669
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-02-25 16:35:05 -0300

    inserido redirecionamentos no template,  caso slug seja invalido

--- devpro/encurtador/facade.py
+++ devpro/encurtador/facade.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 from django.db.models.functions import TruncDate
 
@@ -26,3 +27,17 @@ def localizar_url_reduzida(request, slug):
 
 def localizar_redirects():
     return UrlRedirect.objects.all()
+
+
+def paginizacao(request):
+    parametro_pagina = request.GET.get('page', '1')
+    parametro_limite = '5'
+
+    redirecionamentos = localizar_redirects()
+    redirecionamentos_paginator = Paginator(redirecionamentos, parametro_limite)
+
+    try:
+        page = redirecionamentos_paginator.page(parametro_pagina)
+    except (EmptyPage, PageNotAnInteger):
+        page = redirecionamentos_paginator.page(1)
+    return page
